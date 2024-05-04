71f71b6f8d9c17ba5de43de0f714b5991790249d
Gouravkarnor <gaurav9karnor@gmail.com>
2024-05-04 16:19:36 +0530

    logout fix

--- Backend/Controllers/authentication/logout.js
+++ Backend/Controllers/authentication/logout.js
@@ -1,6 +1,12 @@
 const logout = (req, res) => {
     try {
-        res.clearCookie("userRegistered");
+        res.clearCookie("userRegistered", {
+            // Set the same options used when setting the cookie
+            path: "/", // Ensure the correct path is specified
+            httpOnly: false,
+            secure: true, // Set to true if your server is hosted over HTTPS
+            sameSite: "None", // Ensure SameSite attribute matches the original cookie
+        });
         return res.json({
             status: "success",
             message: "Logout Successful ! ",
